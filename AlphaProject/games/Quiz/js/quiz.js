ac0d7404f9408d150aa98128fda34082582d575d
Love Wikberg <love.wick93@gmail.com>
2017-02-20 09:08:22 +0100

    nu äre dags

--- AlphaProject/games/Quiz/js/quiz.js
+++ AlphaProject/games/Quiz/js/quiz.js
@@ -2,143 +2,143 @@ var questions = [
 {
 question: "Which Turkish city has the name of a cartoon character? ",
 answer: "Batman",
-choice: ["Choose an answer", "Batman", "Gu'lachmet", "Ful al Shem", "Demir"],
+choice: ["Batman", "Gu'lachmet", "Ful al Shem", "Demir"],
 },
 {
 question: "Which is the largest state of the United States?",
 answer: "Alaska",
-choice: ["Choose an answer", "California", "Alaska", "Texas", "North Dakota"],
+choice: [ "California", "Alaska", "Texas", "North Dakota"],
 },
 {
 question: "What is the second largest country in Europe after Russia?",
 answer: "France",
-choice: ["Choose an answer", "Germany", "France", "Ukrain", "Poland"],
+choice: [ "Germany", "France", "Ukrain", "Poland"],
 },
 {
 question: "What is the first book of the Old Testament?",
 answer: "Genesis",
-choice: ["Choose an answer","The protocanonials","Apocrypha","Judas","Genesis"],
+choice: ["The protocanonials","Apocrypha","Judas","Genesis"],
 },
 {
 question: "In which English town did Adolf Hitler study art?",
 answer: "Liverpool",
-choice: ["Choose an answer","London","Brighton","Manchester","Liverpool","North Hampton"],
+choice: ["London","Brighton","Manchester","Liverpool","North Hampton"],
 },
 {
 question: "Who did paint the famous painting Guernica?",
 answer: "Pablo Picasso",
-choice: ["Choose an answer","Leonardo Da Vinci","Pablo Picasso","Silvio Berlusconi","Claudio Marchisio"],
+choice: ["Leonardo Da Vinci","Pablo Picasso","Silvio Berlusconi","Claudio Marchisio"],
 },
 {
 question: "In which city did Romeo and Julia live?",
 answer: "Verona",
-choice: ["Choose an answer","Verona","Palermo","Florence","Naples", "Bologna"],
+choice: ["Verona","Palermo","Florence","Naples", "Bologna"],
 },
 
 {
 question: 'In which italian city is Juventus FCs home ground, "Juventus Stadium", located?',
 answer: "Turin",
-choice: ["Choose an answer","Bologna","Rome","Florence","Turin", "Milano"],
+choice: ["Bologna","Rome","Florence","Turin", "Milano"],
 },
 {
 question: "In 2006, during the football world cup, Zinedine Zidane was sent of for head butting another player. Who was it?",
 answer: "Marco Materazzi",
-choice: ["Choose an answer","Ronaldo","John Terry","Marco Materazzi","Andrea Pirlo", "Paolo Maldini"],
+choice: ["Ronaldo","John Terry","Marco Materazzi","Andrea Pirlo", "Paolo Maldini"],
 },
 {
 question: "How many Grand Slam titles have Roger Federer won?",
 answer: "18",
-choice: ["Choose an answer","10","12","14","16", "18"],
+choice: ["10","12","14","16", "18"],
 },
 {
 question: "Which singer joined Mel Gibson in the movie Mad Max: Beyond The Thunderdome?",
 answer: "Tina Turner",
-choice: ["Choose an answer","Tina Turner","Cher","Nancy Sinatra","Bonnie Tyler"],
+choice: ["Tina Turner","Cher","Nancy Sinatra","Bonnie Tyler"],
 },
 {
 question: "Vodka, Galliano and orange juice are used to make which classic cocktail?",
 answer: "Harvey Wallbanger",
-choice: ["Choose an answer","French 75","Screwdriver","Classic Mimosa","Harvey Wallbanger"],
+choice: ["French 75","Screwdriver","Classic Mimosa","Harvey Wallbanger"],
 },
 {
 question: "On TV, who did the character Lurch work for?",
 answer: "Addams family",
-choice: ["Choose an answer","Burns family","Addams family","The Wahlbergs","The Griffins", "The Cullen clan"],
+choice: ["Burns family","Addams family","The Wahlbergs","The Griffins", "The Cullen clan"],
 },
 {
 question: "What is converted into alcohol during brewing?",
 answer: "Sugar",
-choice: ["Choose an answer","Nitrogen","Hop","Malt","Salt", "Sugar"],
+choice: ["Nitrogen","Hop","Malt","Salt", "Sugar"],
 },
 {
 question: "Which river forms the eastern section of the border between England and Scotland?",
 answer: "Tweed",
-choice: ["Choose an answer","Tweed","Thames","Hudson","Blerch"],
+choice: ["Tweed","Thames","Hudson","Blerch"],
 },
 {
 question: "Name the two families in Romeo and Juliet?",
 answer: "Montague & Capulet",
-choice: ["Choose an answer","Lauren & Capulet","Montague & Capulet","Montague & Lauren","Lauren & Babineaux"],
+choice: ["Lauren & Capulet","Montague & Capulet","Montague & Lauren","Lauren & Babineaux"],
 },
 {
 question: "For which fruit is the US state of Georgia famous?",
 answer: "Peach",
-choice: ["Choose an answer","Pineapple","Mango","Peach","Apricot","Orange"],
+choice: ["Pineapple","Mango","Peach","Apricot","Orange"],
 },
 {
 question: "In which city was Martin Luther King assassinated in 1968?",
 answer: "Memphis, Tennessee",
-choice: ["Choose an answer","Portland, Oregon","St.George, Utah","Memphis, Tennessee"],
+choice: ["Portland, Oregon","St.George, Utah","Memphis, Tennessee"],
 },
 {
 question: "In which country did the Mau Mau uprising (1952-60) occur?",
 answer: "Kenya",
-choice: ["Choose an answer","Kenya","China","Botswana", "Vietnam", "Belize"],
+choice: ["Kenya","China","Botswana", "Vietnam", "Belize"],
 },
 {
 question: "What does a numismatist study or collect?",
 answer: "Coins",
-choice: ["Choose an answer","Stamps","Antiques","Dolls", "Coins"],
+choice: ["Stamps","Antiques","Dolls", "Coins"],
 },
 {
 question: "Which guitarist is known as Slowhand?",
 answer: "Eric Clapton",
-choice: ["Choose an answer","Yngwie Malmsteen","Santana","Jimi Hendrix", "Eric Clapton", "Les Paul"],
+choice: ["Yngwie Malmsteen","Santana","Jimi Hendrix", "Eric Clapton", "Les Paul"],
 },
 {
 question: "Which is the capital of India?",
 answer: "New Delhi",
-choice: ["Choose an answer","Bangladesh","Bombay","Calcutta", "New Delhi"],
+choice: ["Bangladesh","Bombay","Calcutta", "New Delhi"],
 },
 {
 question: "In which film did Roger Moore first play James Bond?",
 answer: "Live and let die",
-choice: ["Choose an answer","Live and let die","The spy who loved me","Moonraker", "Diamonds are forever"],
+choice: ["Live and let die","The spy who loved me","Moonraker", "Diamonds are forever"],
 },
 {
 question: "In which bay is Alcatraz?",
 answer: "San Francisco bay",
-choice: ["Choose an answer","Santa Monica bay","Monterey bay","Alamitos bay", "San Francisco bay", "San Diego bay"],
+choice: ["Santa Monica bay","Monterey bay","Alamitos bay", "San Francisco bay", "San Diego bay"],
 },
 {
 question: "The Merseyside derby is a derby between which two football teams in the Barcleys Premier League?",
 answer: "Everton & Liverpool",
-choice: ["Choose an answer","Liverpool & Manchester United","Everton & Liverpool","Arsenal & Liverpool", "Liverpool & Newcastle"],
+choice: ["Liverpool & Manchester United","Everton & Liverpool","Arsenal & Liverpool", "Liverpool & Newcastle"],
 },
 {
 question: "Who had a 1985 hit with Saving All My Love For You?",
 answer: "Whitney Houston",
-choice: ["Choose an answer","Dolly Parton","Whitney Houston","Celine Dion"],
+choice: ["Dolly Parton","Whitney Houston","Celine Dion"],
 },
 {
 question: "In Roman mythology, Neptune is the equivalent to which Greek god?",
 answer: "Poseidon",
-choice: ["Choose an answer","Poseidon","Ares","Hades","Zeus","Hermes"],
+choice: ["Poseidon","Ares","Hades","Zeus","Hermes"],
 },
 {
 question: "Which of the answers is >NOT< a titan in the Greek mythology?",
 answer: "Eros",
-choice: ["Choose an answer","Cronos","Theia","Oceanus","Atlas","Eros"],
+choice: ["Cronos","Theia","Oceanus","Atlas","Eros"],
 },
 {
 question: "Complete the name of the American Football team: 'Washington ...........'?",
@@ -148,46 +148,69 @@ choice: ["Choose an answer","Penguins","Whitecaps","Redskins","Panthers","Capita
 {
 question: "What is Canada's national animal?",
 answer: "Beaver",
-choice: ["Choose an answer","Beaver","Brown bear","Carcajou","Bald Eagle"],
+choice: ["Beaver","Brown bear","Carcajou","Bald Eagle"],
 },
 {
 question: "Name the largest freshwater lake in the world?",
 answer: "Lake Superior",
-choice: ["Choose an answer","Caspian Sea","Lake Victoria","Lake Tanganyika","Lake Superior"],
+choice: ["Caspian Sea","Lake Victoria","Lake Tanganyika","Lake Superior"],
+},
+{
+question: 'In the movie "Star Wars: The Force Awakens" Han Solo is killed by his son whose name is...?',
+answer: "Kylo Ren",
+choice: ["Rey", "Snoke", "Kylo Ren", "General hux", "Darth Maul", "Anakin Skywalker"],
 },
 {
 question: "What kind of weapon is a falchion?",
 answer: "Sword",
-choice: ["Choose an answer","Rifle","Catapult","Sword"],
+choice: ["Rifle","Catapult","Sword"],
 },
 {
 question: "Name the seventh planet from the sun.",
 answer: "Uranus",
-choice: ["Choose an answer","Neptune","Jupiter","Earth","Uranus","Saturn"],
+choice: ["Neptune","Jupiter","Earth","Uranus","Saturn"],
+},
+{
+question: "Who is this?",
+picture: "img/GwynethPaltrow.jpg",
+answer: "Gwyneth Paltrow",
+choice: ["Jessica Simpson","Elizabeth Hurley","Mandy Moore","Gwyneth Paltrow","Chrissy Teigen"],
 },
 {
 question: "What's the name of this building?",
 picture: "img/tajMahal.jpg",
 answer: "Taj Mahal",
-choice: ["Choose an answer", "Sacré-Coeur", "Hagia Sophia", "Taj Mahal", "Mahabat Maqbara"],
+choice: ["Sacré-Coeur", "Hagia Sophia", "Taj Mahal", "Mahabat Maqbara"],
 },
 {
 question: "Who's this?",
 picture: "img/liamHemsworth.jpg",
 answer: "Liam Hemsworth",
-choice: ["Choose an answer", "Liam Hemsworth", "Ashton Kutcher", "Channing Tatum", "Ian Somerhalder"],
+choice: [ "Liam Hemsworth", "Ashton Kutcher", "Channing Tatum", "Ian Somerhalder"],
 },
 {
 question: "What car is this?",
 picture: "img/camaro.jpg",
 answer: "Chevrolet Camaro",
-choice: ["Choose an answer", "Ford GT", "Pontiac Firebird", "Ford Mustang", "Chevrolet Camaro"],
+choice: ["Ford GT", "Pontiac Firebird", "Ford Mustang", "Chevrolet Camaro"],
 },
 {
 question: "Which company has this logo?",
 picture: "img/motorola.jpg",
 answer: "Motorola",
-choice: ["Choose an answer", "Move", "Motorola", "Migros", "Marley", "Mazda"],
+choice: ["Move", "Motorola", "Migros", "Marley", "Mazda"],
+},
+{
+question: "What are these nuts called?",
+picture: "img/hazelnuts.jpg",
+answer: "Hazelnuts",
+choice: ["Walnuts", "Chestnuts", "Pecans", "Macadamia nuts", "Hazelnuts"],
+},
+{
+question: "In which country are these twin towers located?",
+picture: "img/petronas.jpg",
+answer: "Malaysia",
+choice: ["Kuala Lumpur", "Malaysia", "Hong Kong", "Beijing", "Singapore"],
 },
 ];
 
@@ -197,18 +220,30 @@ var beforePlayersSet = [
 {
 name: "",
 points: 0,
+question: [],
+correct: [],
+answer: [],
 },
 {
 name: "",
 points: 0,
+question: [],
+correct: [],
+answer: [],
 },
 {
 name: "",
 points: 0,
+question: [],
+correct: [],
+answer: [],
 },
 {
 name: "",
 points: 0,
+question: [],
+correct: [],
+answer: [],
 },
 ];
 var playersArray = [];
@@ -251,6 +286,9 @@ function namesToArray(){
 	beforePlayersSet[i].name = getName;
 	beforePlayersSet[i].points = 0;
 	beforePlayersSet[i].print = true;
+	beforePlayersSet[i].question = [];
+	beforePlayersSet[i].answer = [];
+	beforePlayersSet[i].correct = [];
 	playersArray.push(beforePlayersSet[i]);
 	}
 	
@@ -271,6 +309,7 @@ function resetAllValues(){
 }
 
 function playAgain(){
+	document.getElementById("printKey").innerHTML = "<p></p>";
 	resetAllValues();
 	getNumberOfPlayers();
 }
@@ -297,22 +336,23 @@ function startGame(){
 
 function gameCycle(){
 	var html = progressBar();
-	
+	/*html += "<p>Points</p>";
 	html += "<h3>";
 	for(var i=0; i < playersArray.length; i++){
-		html += playersArray[i].name + ": " + playersArray[i].points + "  ";
+		html += "<score>" + playersArray[i].name + ": " + playersArray[i].points + " </score>";
+		document.getElementById("printQuestion").innerHTML = html;
 	}
-	html += "</h3>";
+	html += "</h3>";*/
 	
 	if(questionCounter < (playersArray.length*questionsPerPlayer))
 	{	
-		html += "<p> <mark>" + playersArray[playerTurn].name + ", it's your turn!</mark></p><br>";
+		if(playersArray.length > 1)
+		html += "<p><mark><b>" + playersArray[playerTurn].name + ", it's your turn!</b></mark></p><br>";
 		
 		var randomNumber = Math.floor((Math.random() * questions.length) + 1);	
 		randomNumber -= 1;		
 		html += printQuestion(randomNumber);		
-		var category = 'onclick="calculatePoints('+randomNumber+')"';		
-		html += '<br>' + '<input type="button" class="buttonDesign" value="Next"' + category + '<br>' + ' </input>';
+		html += '<br>' + '<input type="button" class="buttonDesign" value="Next" onclick="calculatePoints('+randomNumber+')"<br></input>';
 		document.getElementById("printQuestion").innerHTML = html;
 		var percentProgress = ((questionCounter) / (playersArray.length*questionsPerPlayer))*100;
 		document.getElementById("myBar").style.width = percentProgress+"%"
@@ -331,7 +371,7 @@ function gameCycle(){
 		if(playersArray.length == 1){
 				var percent = convertToPercent(0);
 				gameEnd += "<p>Points: " + playersArray[0].points+ "/"+questionsPerPlayer+"</p>";
-				gameEnd += '<p>You got '+percent.toString()+'% of the answers right.</p>';
+				gameEnd += '<p>You got '+percent.toString()+'% of the answers right.</p><input type="button" value="Show answers" onclick="printKey(0)"</input>';
 		}
 		else
 		{	
@@ -356,16 +396,26 @@ function gameCycle(){
 					if(playersArray[i].points == pointArray[(ind-1)] && playersArray[i].print == true){
 						playersArray[i].print = false;
 						var percent = convertToPercent(i);
-						gameEnd += "<p>" +playersArray[i].name+ "    Points: " + playersArray[i].points+ "/"+questionsPerPlayer+" ("+percent+"%)</p>";
+						gameEnd += '<p>' +playersArray[i].name+ '    Points: ' + playersArray[i].points+ '/'+questionsPerPlayer+' ('+percent+'%)</p><input type="button" value="Show answers" onclick="printKey('+i+')"</input>';
 					}
 				}
 				ind-=1;
 			}
 		}
 		
-		gameEnd += '<input type="button" class="buttonDesign" value="Play again" onclick="playAgain()"></input>'
+		gameEnd += '<br><br><input type="button" class="buttonDesign" value="Play again" onclick="playAgain()"></input>'
 		document.getElementById("printQuestion").innerHTML = gameEnd;
 	}
+}
+
+function printKey(index){
+	html = "";
+	for(var i=0; i < playersArray[index].question.length; i++){
+			html += "<hr><p>" +playersArray[index].question[i]+"</p>";
+			html += "<p>Correct answer: " +playersArray[index].correct[i]+"</p>";
+			html += "<p>Your answer: " +playersArray[index].answer[i]+"</p>";
+		}
+	document.getElementById("printKey").innerHTML = html;
 }
 
 function printQuestion(index){
@@ -382,8 +432,9 @@ function printQuestion(index){
 	}
 	
 	html += '<select text="hej" id="answer">';
+	html += '<option>Choose an answer</option>';
 	for(var x=0; x < questions[index].choice.length; x++){
-		html += '<option value="' +questions[index].choice[x]+ '">' +questions[index].choice[x]+ '</option>' 
+		html += '<option value="' +questions[index].choice[x]+ '" >' +questions[index].choice[x]+ '</option>' 
 	}
 	html += "</select><br>";
 	return html;
@@ -395,6 +446,10 @@ function calculatePoints(index){
 	{
 		playersArray[playerTurn].points += 1;
 	}
+	playersArray[playerTurn].question.push(questions[index].question);
+	playersArray[playerTurn].correct.push(questions[index].answer);
+	playersArray[playerTurn].answer.push(value);
+	
 	if(playerTurn < (playersArray.length-1))
 		playerTurn += 1;
 	else if (playerTurn === (playersArray.length-1))
